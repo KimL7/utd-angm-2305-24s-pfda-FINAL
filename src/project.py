1c63d4de6b9f6d0799803abad2e4ae4ef8e666ba
Elect <kiml77077@gmail.com>
2024-04-22 20:43:14 -0500

    feat: implemented turtle capabilities and click counter features.

--- src/project.py
+++ src/project.py
@@ -1,13 +1,30 @@
 import turtle
 
-wn = turtle.Screen()
-wn.title("Cat Expression!")
-wn.bgcolor("white")
+display = turtle.Screen()
+display.title("Cat Expression!") #Title of program
+display.bgcolor("white")
 
-wn.register_shape("placeholder.gif")
+# Update expressions later
+display.register_shape("placeholder.gif") #image
 
 cat = turtle.Turtle()
-cat.shape("placeholder.gif")
+cat.shape("placeholder.gif") #image
 cat.speed(0)
 
-wn.mainloop()
+click = 0
+
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.color("black")
+pen.penup()
+pen.goto(0, 250)
+pen.write(f"Clicks: {click}", align="center", font=("Verdana", 40, "normal")) #text
+
+def clicked(x, y):
+    global click
+    click += 1
+    pen.clear()
+    pen.write(f"Clicks: {click}", align="center", font=("Verdana", 40, "normal")) #text
+cat.onclick(clicked)
+
+display.mainloop()
